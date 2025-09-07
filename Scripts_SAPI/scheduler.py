f682cdda75796f9bcbd96f0da56ee8b04606d872
RGB39 <79316659+reallyGoodBaker@users.noreply.github.com>
2025-09-07 21:56:54 +0800

    update: promise

--- Scripts_SAPI/scheduler.py
+++ Scripts_SAPI/scheduler.py
@@ -142,7 +142,7 @@ class Future:
         self._executor.start()
 
     def wait(self):
-        # type: () -> tuple[InstanceType, Exception]
+        # type: () -> tuple[Any, Exception]
         self._executor.join()
         return self._value, self._error
 
